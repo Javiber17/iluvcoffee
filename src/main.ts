debb5200904ec3fc63131adb85e18365f5ec7c95
Javiber17 <bernal.a.javier@gmail.com>
2023-06-25 23:26:18 -0300

    nest

--- src/main.ts
+++ src/main.ts
@@ -4,10 +4,13 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe({
-    whitelist: true,
-    transform: true,
-    forbidNonWhitelisted: true}),);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      transform: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
   await app.listen(3000);
 }
 bootstrap();
