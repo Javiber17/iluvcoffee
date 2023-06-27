a818f4f82804c44df5cfdfa8e83f33dfbd05261e
Javier B <85018999+Javiber17@users.noreply.github.com>
2023-06-26 23:02:06 -0300

    Update update-coffee.dto.ts
    prueba

--- src/coffees/dto/update-coffee.dto.ts
+++ src/coffees/dto/update-coffee.dto.ts
@@ -3,4 +3,4 @@ import { CreateCoffeeDto } from "./create-coffee.dto";
 
 export class UpdateCoffeeDto extends PartialType(CreateCoffeeDto) {}
 
-//Minuto 6:00 en video 17......
+//Minuto 6:00 en video 17..
